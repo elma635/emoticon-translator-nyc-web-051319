a200509f014e5da69bbce95b8610b5c671813d17
Elma Rahman <elmarahman635@gmail.com>
2019-05-03 01:42:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(file_path) #arg is taking in a string, so it's using whatevers 
     translator = {get_meaning: {}, get_emoticon: {}}
       emoticons.each do |meaning, emoticon_symbols| #iterate over resulting hash (emoticons)
           translator[:get_meaning][emoticon_symbols[1]] = meaning #when you make the key you have to set it equal to something, to make it populate
-          translator[:get_emoticon][emoticon_symbols[2]] = emoticon_symbols
+          #translator[:get_emoticon][emoticon_symbols[2]] = emoticon_symbols
  #binding.pry
 
           #this is how we want it to look:
@@ -25,20 +25,20 @@ def load_library(file_path) #arg is taking in a string, so it's using whatevers 
   translator
 end
 
-
-def get_japanese_emoticon(file_path, emoticon)
-  #loaded_library = load_library(file_path) -- dont need this bc the line under is doing that already
-    load_library = YAML.load_file(file_path)
-
-
-end
-
-def get_english_meaning(file_path, emoticon)
-  load_library = YAML.load_file(file_path)
-    load_library.each do |meaning, emoticon_symbols|
-
-      if meaning[:grinning] =
-
-    end
-  end
-end 
+# 
+# def get_japanese_emoticon(file_path, emoticon)
+#   #loaded_library = load_library(file_path) -- dont need this bc the line under is doing that already
+#     load_library = YAML.load_file(file_path)
+# 
+# 
+# end
+# 
+# def get_english_meaning(file_path, emoticon)
+#   load_library = YAML.load_file(file_path)
+#     load_library.each do |meaning, emoticon_symbols|
+# 
+#       if meaning[:grinning] =
+# 
+#     end
+#   end
+# end 
