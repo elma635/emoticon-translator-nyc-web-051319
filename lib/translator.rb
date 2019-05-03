bffc157d16f8bb5d389f3aadc51e3c364e8ccfed
Elma Rahman <elmarahman635@gmail.com>
2019-05-03 01:51:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,10 +4,10 @@ require 'pry'
 # require modules here
 def load_library(file_path) #arg is taking in a string, so it's using whatevers in the .yml file so file_path
   emoticons = YAML.load_file(file_path) #we don't have to use path.yml because calling . on anything = calling method. so call on the file path which refers to info in emoticons.yml
-    translator = {get_meaning: {}, get_emoticon: {}}
+    translator = {"get_meaning" => {}, "get_emoticon" => {}}
       emoticons.each do |meaning, emoticon_symbols| #iterate over resulting hash (emoticons)
-          translator[:get_meaning][emoticon_symbols[1]] = meaning #when you make the key you have to set it equal to something, to make it populate
-          translator[:get_emoticon][emoticon_symbols[0]] = emoticon_symbols[1]
+          translator["get_meaning"][emoticon_symbols[1]] = meaning #when you make the key you have to set it equal to something, to make it populate
+          translator["get_emoticon"][emoticon_symbols[0]] = emoticon_symbols[1]
  #binding.pry
 
           #this is how we want it to look:
